82dcb110e64d66d30aa962e3e0d8043a971f2608
Methode-virtuelle-pur <antoine.anquetil@ensg.eu>
2025-01-28 11:16:06 +0100

    first

---
+++ sw.js
@@ -0,0 +1,33 @@
+// sw.js
+
+// install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('nom_du_cache')
+        .then((cache) => {
+          return cache.addAll([
+            './',
+            'pwa.html',
+            'stylesheets/styles.css',
+            'images/a.jpg',
+            'images/b.jpg',
+            'images/c.jpg',
+            'images/d.jpg',
+            'manifest.json'
+         ]);
+        })
+        .then(() => {
+          return self.skipWaiting();
+        })
+    );
+  });
+  
+// fetch event
+self.addEventListener('fetch', (event) => {
+event.respondWith(
+    caches.match(event.request).then((response) => {
+    return response || fetch(event.request);
+    })
+);
+});
+  
